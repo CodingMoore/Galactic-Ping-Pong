7f846861b5e578c5bcaebee8d9b1519e21d26cb8
Randel M <Randel.C.Moore@gmail.com>
2022-02-11 06:23:13 -0800

    Adds footer, panzoom reset button, and some styling

--- js/scripts.js
+++ js/scripts.js
@@ -71,18 +71,25 @@ $(function() {
   });
   
   
-    /////////////////////////
-    ///Scroll to Div Logic///
-    /////////////////////////
-    $('a[href*=\\#]:not([href=\\#])').on('click', function() {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
-      if (target.length) {
-          $('html,body').animate({
-              scrollTop: target.offset().top
-          }, 1000);
-          return false;
-      }
-    });
+  /////////////////////////
+  ///Scroll to Div Logic///
+  /////////////////////////
+  $('a[href*=\\#]:not([href=\\#])').on('click', function() {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
+    if (target.length) {
+        $('html,body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+        return false;
+    }
+  });
+
+  /////////////////////////
+  ///Reset Panzoom Logic///
+  /////////////////////////
+  $("#resetPanZoomButton").click(function() {
+    panzoom.reset();
+  });
 
 });
