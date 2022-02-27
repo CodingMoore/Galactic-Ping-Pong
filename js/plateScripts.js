1754d0985653dcc1af9eeda49bd30668ce527410
Randel M <Randel.C.Moore@gmail.com>
2022-02-26 17:13:41 -0800

    Fixes plate flip functionality

--- js/plateScripts.js
+++ js/plateScripts.js
@@ -9,15 +9,33 @@ $(function() {
   });
 
 
-  // Plate Flip
+  //////////////////////
+  ///Plate Flip Logic///
+  //////////////////////
   $("#plateFlipButton").click(function() {
 
+    // Triggers a plate reset so that the flip animates
+    panzoom.reset();
+
+
+
+    // let xScale = panzoom.getScale(5);
+    // console.log(xScale);
+
+    // Panzoom(element, {
+    //   setTransform: (_, { scale, x, y }) => {
+    //     panzoom.setStyle('transform', `scale(-${scale},${scale})`)
+    //   }
+    // });
+
+    //Does the flipping
     if($("#svgWrapper").hasClass("plateflipped")) {
-      $("#svgWrapper").css("transform", "scale(1,1)");
+      //$("#svgWrapper").css("transform", "scale(1,1)");
       $("#svgWrapper").removeClass("plateflipped")
     } 
     else {
-      $("#svgWrapper").css("transform", "scale(-1,1)");
+      //$("#svgWrapper").css("transform", "scale(-1,1)");
+      //$("#svgImage").css("transform-origin", "50% -50%");
       $("#svgWrapper").addClass("plateflipped")
     }
     
