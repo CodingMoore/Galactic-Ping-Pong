5520e09d7139cd44bab80d250e954fe0fdbc6bfa
Randel M <Randel.C.Moore@gmail.com>
2022-03-03 07:02:03 -0800

    Updates styling for plate header buttons

--- js/plateScripts.js
+++ js/plateScripts.js
@@ -17,12 +17,14 @@ $(function() {
     // Triggers a plate reset so that the flip animates
     panzoom.reset();
 
-    // Does the flipping
+    // Does the flipping and highlight
     if($("#svgWrapper").hasClass("plateflipped")) {
-      $("#svgWrapper").removeClass("plateflipped")
+      $("#svgWrapper").removeClass("plateflipped");
+      $("#plateFlipButton").css("background-color", "lightgray");
     } 
     else {
-      $("#svgWrapper").addClass("plateflipped")
+      $("#svgWrapper").addClass("plateflipped");
+      $("#plateFlipButton").css("background-color", "rgb(134, 134, 134)");
     }
     
   });
@@ -37,10 +39,12 @@ $(function() {
     if($("#skyImageBox").hasClass("skyImageOn")) {
       $("#skyImageBox").removeClass("skyImageOn");
       $("#skyImageBox").hide();
+      $("#backgroundImageButton").css("background-color", "lightgray");
     }
     else {
       $("#skyImageBox").addClass("skyImageOn");
       $("#skyImageBox").show();
+      $("#backgroundImageButton").css("background-color", "rgb(134, 134, 134)");
     }
 
     if($("#backgroundFill").hasClass("fillOn")) {
